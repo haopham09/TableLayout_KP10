f807bdcb9337f650ec12bd46adc182c425dde2ab
Hao Pham <phamvanhao009@gmail.com>
2019-10-29 17:01:57 +0700

    First commit

---
+++ app/src/main/java/com/thohao/tablelayout_kp09/SecondActivity.java
@@ -0,0 +1,14 @@
+package com.thohao.tablelayout_kp09;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
